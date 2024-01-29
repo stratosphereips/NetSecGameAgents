3fda70f034da7c2de3d5e4938e1475f416344a48
Ondrej Lukas <ondrej.lukas95@gmail.com>
2024-01-29 19:08:33 +0100

    Add state_to_string method

--- agents/agent_utils.py
+++ agents/agent_utils.py
@@ -1,32 +1,54 @@
+"""
+Collection of functions which are intended for agents to share.
+Functionality which is specific for a certain type of agent should be implementented
+directly in the agent class. 
+
+author: Ondrej Lukas - ondrej.lukas@aic.fel.cvut.cz
+"""
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname( os.path.abspath(__file__) )))
 #with the path fixed, we can import now
 from env.game_components import Action, ActionType, GameState, Observation
 
-
 def generate_valid_actions(state: GameState)->list:
-        valid_actions = set()
-        for src_host in state.controlled_hosts:
-            #Network Scans
-            for network in state.known_networks:
-                # TODO ADD neighbouring networks
-                valid_actions.add(Action(ActionType.ScanNetwork, params={"target_network": network, "source_host": src_host,}))
-            # Service Scans
-            for host in state.known_hosts:
-                valid_actions.add(Action(ActionType.FindServices, params={"target_host": host, "source_host": src_host,}))
-            # Service Exploits
-            for host, service_list in state.known_services.items():
-                for service in service_list:
-                    valid_actions.add(Action(ActionType.ExploitService, params={"target_host": host,"target_service": service,"source_host": src_host,}))
-        # Data Scans
-        for host in state.controlled_hosts:
-            valid_actions.add(Action(ActionType.FindData, params={"target_host": host, "source_host": host}))
+    """Function that generates a list of all valid actions in a given state"""
+    valid_actions = set()
+    for src_host in state.controlled_hosts:
+        #Network Scans
+        for network in state.known_networks:
+            # TODO ADD neighbouring networks
+            valid_actions.add(Action(ActionType.ScanNetwork, params={"target_network": network, "source_host": src_host,}))
+        # Service Scans
+        for host in state.known_hosts:
+            valid_actions.add(Action(ActionType.FindServices, params={"target_host": host, "source_host": src_host,}))
+        # Service Exploits
+        for host, service_list in state.known_services.items():
+            for service in service_list:
+                valid_actions.add(Action(ActionType.ExploitService, params={"target_host": host,"target_service": service,"source_host": src_host,}))
+    # Data Scans
+    for host in state.controlled_hosts:
+        valid_actions.add(Action(ActionType.FindData, params={"target_host": host, "source_host": host}))
 
-        # Data Exfiltration
-        for src_host, data_list in state.known_data.items():
-            for data in data_list:
-                for trg_host in state.controlled_hosts:
-                    if trg_host != src_host:
-                        valid_actions.add(Action(ActionType.ExfiltrateData, params={"target_host": trg_host, "source_host": src_host, "data": data}))
-        return list(valid_actions)   
+    # Data Exfiltration
+    for src_host, data_list in state.known_data.items():
+        for data in data_list:
+            for trg_host in state.controlled_hosts:
+                if trg_host != src_host:
+                    valid_actions.add(Action(ActionType.ExfiltrateData, params={"target_host": trg_host, "source_host": src_host, "data": data}))
+    return list(valid_actions)
+
+def state_as_ordered_string(state:GameState)->str:
+    """Function for generating string representation of a SORTED gamestate components. Can be used as key for dictionaries."""
+    ret = ""
+    ret += f"nets:[{','.join([str(x) for x in sorted(state.known_networks)])}],"
+    ret += f"hosts:[{','.join([str(x) for x in sorted(state.known_hosts)])}],"
+    ret += f"controlled:[{','.join([str(x) for x in sorted(state.controlled_hosts)])}],"
+    ret += "services:{"
+    for host in sorted(state.known_services.keys()):
+        ret += f"{host}:[{','.join([str(x) for x in sorted(state.known_services[host])])}]"
+    ret += "},data:{"
+    for host in sorted(state.known_data.keys()):
+        ret += f"{host}:[{','.join([str(x) for x in sorted(state.known_data[host])])}]"
+    ret += "}"
+    return ret   
