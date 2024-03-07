40813da834faa9c305cd8257531ce3ebf9ece26f
Sebastian Garcia <eldraco@gmail.com>
2024-03-07 22:51:01 +0100

    check-q-table. Add colors and print equal values

--- agents/q_learning/check_q_table.py
+++ agents/q_learning/check_q_table.py
@@ -1,5 +1,7 @@
 import argparse
+from math import inf
 import pickle
+from colorama import Fore, Style, init
 
 #q_values = {}
 #states = {}
@@ -18,15 +20,27 @@ def show_q_table():
     Show details about a state in the qtable
     """
     print(f'State: {list(states.items())[args.state_id]}')
-    for ((id, action), value) in list(q_values.items()):
-        if int(id)==args.state_id:
-            print(f'{id}: {action} -> {value}')
+
+    filtered_items = {key: value for key, value in q_values.items() if key[0] == args.state_id}
+
+    sorted_items = dict(sorted(filtered_items.items(), key=lambda item: item[1], reverse=True))
+
+    # Identify the maximum value
+    max_value = next(iter(sorted_items.values()), None)
+
+    for index, (key, value) in enumerate(sorted_items.items()):
+        if value == max_value:
+            print(Fore.RED + f'{key} -> {value}')
+        else:
+            print(Fore.GREEN + f'{key} -> {value}')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('You can train the agent, or test it. \n Test is also to use the agent. \n During training and testing the performance is logged.')
     parser.add_argument("--file", help="Q-table file to load", default="q_agent_marl.pickle", required=False, type=str)
     parser.add_argument("--state_id", help="ID of the state to print", default=0, required=False, type=int)
     args = parser.parse_args()
+
+    init(autoreset=True)
 
     load_q_table()
 
