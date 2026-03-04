7dcd242cc9555cf3e0f68ad4176f852cd578f4c0
Ondrej Lukas <ondrej.lukas95@gmail.com>
2026-03-04 18:04:49 +0100

    remove mlflow support

--- agents/attackers/random/random_agent.py
+++ agents/attackers/random/random_agent.py
@@ -3,7 +3,6 @@
 import logging
 import argparse
 import numpy as np
-import mlflow
 from os import path, makedirs
 import random
 from netsecgame import Action, Observation, BaseAgent, generate_valid_actions, AgentRole
@@ -49,7 +48,6 @@ def main():
     parser.add_argument("--episodes", help="Sets number of episodes to play", default=100, type=int)
     parser.add_argument("--seed", help="Sets random seed for agent's decisions", default=42, type=int) 
     parser.add_argument("--logdir", help="Folder to store logs", default=path.join(path.dirname(path.abspath(__file__)), "logs"))
-    parser.add_argument("--mlflow_url", help="URL for mlflow tracking server. If not provided, mlflow will store locally.", default=None)
     args = parser.parse_args()
 
     if not path.exists(args.logdir):
@@ -58,118 +56,95 @@ def main():
 
     # Create agent
     agent = RandomAttackerAgent(args.host, args.port, AgentRole.Attacker, seed=args.seed)
-
-    # Mlflow experiment name        
-    experiment_name = "Random Attacker Agent"
-    if args.mlflow_url:
-        mlflow.set_tracking_uri(args.mlflow_url)
-    mlflow.set_experiment(experiment_name)
     
     # Register in the game
     observation = agent.register()
+    observation = agent.request_game_reset(randomize_topology=True, seed=0)
     
-    with mlflow.start_run(run_name=experiment_name) as run:
-        # To keep statistics of each episode
-        wins = 0
-        detected = 0
-        max_steps = 0
-        num_win_steps = []
-        num_detected_steps = []
-        num_max_steps_steps = []
-        num_detected_returns = []
-        num_win_returns = []
-        num_max_steps_returns = []
 
-        # Log more things in Mlflow
-        mlflow.set_tag("experiment_name", experiment_name)
-        mlflow.set_tag("episode_number", args.episodes)
+    # To keep statistics of each episode
+    wins = 0
+    detected = 0
+    max_steps = 0
+    num_win_steps = []
+    num_detected_steps = []
+    num_max_steps_steps = []
+    num_detected_returns = []
+    num_win_returns = []
+    num_max_steps_returns = []
 
-        for episode in range(1, args.episodes + 1):
-            agent.logger.info(f'Starting episode {episode}')
-            print(f'Starting episode {episode}')
 
-            # Play the game for one episode
-            episodic_returns = []
-            num_steps = 0
-            
-            while observation and not observation.end:
-                num_steps += 1
-                agent.logger.debug(f'Observation received:{observation}')
-                # Store returns in the episode
-                episodic_returns.append(observation.reward)
-                # Select the action randomly
-                action = agent.select_action(observation)
-                observation = agent.make_step(action)
-            
-            agent.logger.debug(f'Observation received:{observation}')
-            current_return = np.sum(episodic_returns)
-            
-            agent.logger.info(f"Episode {episode} ended with return {current_return}.")
-            
-            reward = current_return
-            
-            if observation.info and observation.info.get('end_reason') == AgentStatus.Fail:
-                detected +=1
-                num_detected_steps.append(num_steps)
-                num_detected_returns.append(reward)
-            elif observation.info and observation.info.get('end_reason') == AgentStatus.Success:
-                wins += 1
-                num_win_steps.append(num_steps)
-                num_win_returns.append(reward)
-            elif observation.info and observation.info.get('end_reason') == AgentStatus.TimeoutReached:
-                max_steps += 1
-                num_max_steps_steps.append(num_steps)
-                num_max_steps_returns.append(reward)
+    for episode in range(1, args.episodes + 1):
+        agent.logger.info(f'Starting episode {episode}')
+        print(f'Starting episode {episode}')
 
-            # Reset the game - ONLY ONCE
-            if episode < args.episodes:
-                 observation = agent.request_game_reset()
-
-            # Calculate stats for logging
-            eval_win_rate = (wins/episode) * 100
-            eval_detection_rate = (detected/episode) * 100
-            
-            all_returns = num_detected_returns + num_win_returns + num_max_steps_returns
-            eval_average_returns = np.mean(all_returns) if all_returns else 0
-            eval_std_returns = np.std(all_returns) if all_returns else 0
-            
-            all_steps = num_win_steps + num_detected_steps + num_max_steps_steps
-            eval_average_episode_steps = np.mean(all_steps) if all_steps else 0
-            eval_std_episode_steps = np.std(all_steps) if all_steps else 0
-
-            # Store in mlflow
-            mlflow.log_metric("win_rate", eval_win_rate, step=episode)
-            mlflow.log_metric("detection_rate", eval_detection_rate, step=episode)
-            mlflow.log_metric("avg_returns", eval_average_returns, step=episode)
-            mlflow.log_metric("std_returns", eval_std_returns, step=episode)
-            mlflow.log_metric("avg_episode_steps", eval_average_episode_steps, step=episode)
+        # Play the game for one episode
+        episodic_returns = []
+        num_steps = 0
         
-        # Log the last final episode when it ends
-        text = f'''Final results for {args.episodes} episodes:
-            Wins={wins},
-            Detections={detected},
-            MaxSteps={max_steps},
-            winrate={eval_win_rate:.3f}%,
-            detection_rate={eval_detection_rate:.3f}%,
-            average_returns={eval_average_returns:.3f} +- {eval_std_returns:.3f},
-            average_episode_steps={eval_average_episode_steps:.3f} +- {eval_std_episode_steps:.3f}
-            '''
+        while observation and not observation.end:
+            num_steps += 1
+            agent.logger.debug(f'Observation received:{observation}')
+            # Store returns in the episode
+            episodic_returns.append(observation.reward)
+            # Select the action randomly
+            action = agent.select_action(observation)
+            observation = agent.make_step(action)
+        
+        agent.logger.debug(f'Observation received:{observation}')
+        current_return = np.sum(episodic_returns)
+        
+        agent.logger.info(f"Episode {episode} ended with return {current_return}.")
+        
+        reward = current_return
+        
+        if observation.info and observation.info.get('end_reason') == AgentStatus.Fail:
+            detected +=1
+            num_detected_steps.append(num_steps)
+            num_detected_returns.append(reward)
+        elif observation.info and observation.info.get('end_reason') == AgentStatus.Success:
+            wins += 1
+            num_win_steps.append(num_steps)
+            num_win_returns.append(reward)
+        elif observation.info and observation.info.get('end_reason') == AgentStatus.TimeoutReached:
+            max_steps += 1
+            num_max_steps_steps.append(num_steps)
+            num_max_steps_returns.append(reward)
 
-        agent.logger.info(text)
-        print(text)
-        agent._logger.info("Terminating interaction")
-        agent.terminate_connection()
+        # Reset the game - ONLY ONCE
+        if episode < args.episodes:
+            if episode % 10 == 0:
+                observation = agent.request_game_reset(randomize_topology=True, seed=episode)
+            else:
+                observation = agent.request_game_reset(randomize_topology=False, seed=episode)
 
-        # Print and log the mlflow experiment ID, run ID, and storage location
-        experiment_id = run.info.experiment_id
-        run_id = run.info.run_id
-        storage_location = "locally" if not args.mlflow_url else f"at {args.mlflow_url}"
-        print(f"MLflow Experiment ID: {experiment_id}")
-        print(f"MLflow Run ID: {run_id}")
-        print(f"Experiment saved {storage_location}")
-        agent._logger.info(f"MLflow Experiment ID: {experiment_id}")
-        agent._logger.info(f"MLflow Run ID: {run_id}")
-        agent._logger.info(f"Experiment saved {storage_location}")
+        # Calculate stats for logging
+        eval_win_rate = (wins/episode) * 100
+        eval_detection_rate = (detected/episode) * 100
+        
+        all_returns = num_detected_returns + num_win_returns + num_max_steps_returns
+        eval_average_returns = np.mean(all_returns) if all_returns else 0
+        eval_std_returns = np.std(all_returns) if all_returns else 0
+        
+        all_steps = num_win_steps + num_detected_steps + num_max_steps_steps
+        eval_average_episode_steps = np.mean(all_steps) if all_steps else 0
+        eval_std_episode_steps = np.std(all_steps) if all_steps else 0
+    
+    # Log the last final episode when it ends
+    text = f'''Final results for {args.episodes} episodes:
+        Wins={wins},
+        Detections={detected},
+        MaxSteps={max_steps},
+        winrate={eval_win_rate:.3f}%,
+        detection_rate={eval_detection_rate:.3f}%,
+        average_returns={eval_average_returns:.3f} +- {eval_std_returns:.3f},
+        average_episode_steps={eval_average_episode_steps:.3f} +- {eval_std_episode_steps:.3f}
+        '''
+
+    agent.logger.info(text)
+    print(text)
+    agent._logger.info("Terminating interaction")
+    agent.terminate_connection()
 
 if __name__ == '__main__':
     main()
